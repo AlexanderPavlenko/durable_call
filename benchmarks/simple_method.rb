2dffc01d49e07460c623a880ef2317a4a5f9f618
Alexander Pavlenko <apavlenko@mirantis.com>
2013-04-23 12:47:36 +0400

    benchmark for caller

--- benchmarks/simple_method.rb
+++ benchmarks/simple_method.rb
@@ -4,10 +4,17 @@ require File.expand_path('../../lib/durable_call.rb', __FILE__)
 n = 500000
 Benchmark.bm do |bm|
   object = Object.new
+  caller = DurableCall::Caller.new(object)
 
   bm.report do
     n.times do
       object.__send__ :object_id
+    end
+  end
+
+  bm.report do
+    n.times do
+      caller.call(:object_id)
     end
   end
 
